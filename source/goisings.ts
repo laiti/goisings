1362bdcae27be5be5d4e5ffe15175da7c5bcc50e
Antti Laiti <antti@laiti.fi>
2022-04-08 18:42:14 +0300

    add telegram-bot-api module and adjust prettier

--- source/goisings.ts
+++ source/goisings.ts
@@ -7,12 +7,15 @@ import telegramConfig from '../config/telegram.json';
 const clap = new ClapDetector(clapConfig);
 
 function ts() {
-    if (clapConfig.TIMESTAMP) {
-        return moment().format('YYYY-MM-DDTHH:mm:ss: ');
-    }
-    return '';
+  if (clapConfig.TIMESTAMP) {
+    return moment().format('YYYY-MM-DDTHH:mm:ss: ');
+  }
+  return '';
 }
 
-const disposableTwoClapsListener = clap.addClapsListener(claps => {
-    console.log("heard 2 claps", claps)
-  }, { number: clapConfig.CLAPS, delay: clapConfig.TIMEOUT });
+const disposableTwoClapsListener = clap.addClapsListener(
+  (claps) => {
+    console.log(`${ts()} heard 2 claps`, claps);
+  },
+  { number: clapConfig.CLAPS, delay: clapConfig.TIMEOUT },
+);
