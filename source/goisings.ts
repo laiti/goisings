35c5551413331ed022c9ed37c7e95aa0ee780bf6
Antti Laiti <antti@laiti.fi>
2022-04-08 18:46:39 +0300

    add telegram bot messaging

--- source/goisings.ts
+++ source/goisings.ts
@@ -1,5 +1,6 @@
-import ClapDetector from 'clap-detector';
 import moment from 'moment';
+import TelegramBot from 'node-telegram-bot-api';
+import ClapDetector from 'clap-detector';
 
 import clapConfig from '../config/clap.json';
 import telegramConfig from '../config/telegram.json';
@@ -13,9 +14,12 @@ function ts() {
   return '';
 }
 
+const bot = new TelegramBot(telegramConfig.botToken, { polling: false });
+
 const disposableTwoClapsListener = clap.addClapsListener(
   (claps) => {
     console.log(`${ts()} heard 2 claps`, claps);
+    bot.sendMessage(telegramConfig.chatId, 'sticker');
   },
   { number: clapConfig.CLAPS, delay: clapConfig.TIMEOUT },
 );
