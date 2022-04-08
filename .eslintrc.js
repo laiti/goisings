4aa1bf7995c413f05f52ebdf7747929173c13247
Antti Laiti <antti@laiti.fi>
2022-04-08 18:37:08 +0300

    remove merged typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   extends: [
     'prettier',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   plugins: ['prettier'],
